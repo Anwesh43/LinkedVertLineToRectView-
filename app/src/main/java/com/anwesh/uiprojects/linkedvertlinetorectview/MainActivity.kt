ffba6ac3ff607780baa5fe2d4cbddda52796e71e
Anwesh43 <anweshthecool0@gmail.com>
2020-02-09 20:03:38 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedvertlinetorectview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedvertlinetorectview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedvertlinetorectview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.vertlinetorectview.VertLineToRectView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        VertLineToRectView.create(this)
     }
 }
