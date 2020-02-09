7f3fe816bfd7a251a3343332039f8d0ae24f580a
Anwesh43 <anweshthecool0@gmail.com>
2020-02-09 20:40:25 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedvertlinetorectview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedvertlinetorectview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedvertlinetorectview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.vertlinetorectview.VertLineToRectView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         VertLineToRectView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
