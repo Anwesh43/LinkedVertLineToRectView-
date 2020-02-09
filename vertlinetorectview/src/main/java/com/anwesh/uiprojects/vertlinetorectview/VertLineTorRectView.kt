59b10aa01fb3d53559c8ab71c71fe9e1c03c699b
Anwesh43 <anweshthecool0@gmail.com>
2020-02-09 19:44:41 +0530

    adding Canvas extension methods

--- vertlinetorectview/src/main/java/com/anwesh/uiprojects/vertlinetorectview/VertLineTorRectView.kt
+++ vertlinetorectview/src/main/java/com/anwesh/uiprojects/vertlinetorectview/VertLineTorRectView.kt
@@ -20,6 +20,9 @@ val strokeFactor : Int = 90
 val delay : Long = 20
 val foreColor : Int = Color.parseColor("#673AB7")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val hFactor : Float = 2f
+val wFactor : Float = 1f
+val sizeFactor : Float = 2.9f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -35,3 +38,37 @@ fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
 //    0.3f * 2 -> 0.6
 //}
 
+fun Canvas.drawVertRect(scale : Float, size : Float, paint : Paint) {
+    val rw : Float = size / wFactor
+    val rh : Float = size / hFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, 2)
+    val sf2 : Float = sf.divideScale(1, 2)
+    val path : Path = Path()
+    val x1 : Float = -rw / 2
+    val x2 : Float = -rw / 2 + rw * sf1
+    val x3 : Float = -rw / 2 + rw * sf2
+    val y1 : Float = -rh / 2
+    val y2 : Float = rh / 2
+    paint.style = Paint.Style.STROKE
+    path.moveTo(x1, y2)
+    path.lineTo(x1, y1)
+    path.lineTo(x2, y1)
+    path.lineTo(x3, y2)
+    path.lineTo(x1, y2)
+    drawPath(path, paint)
+}
+
+fun Canvas.drawVLTRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawVertRect(scale, size, paint)
+    restore()
+}
