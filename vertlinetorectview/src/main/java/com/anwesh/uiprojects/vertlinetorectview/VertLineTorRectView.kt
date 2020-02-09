cb88a946af4bad920ecc982ecb8114d7d77e0056
Anwesh43 <anweshthecool0@gmail.com>
2020-02-09 19:17:21 +0530

    adding constants and libraries

--- vertlinetorectview/src/main/java/com/anwesh/uiprojects/vertlinetorectview/VertLineTorRectView.kt
+++ vertlinetorectview/src/main/java/com/anwesh/uiprojects/vertlinetorectview/VertLineTorRectView.kt
@@ -4,3 +4,20 @@ package com.anwesh.uiprojects.vertlinetorectview
  * Created by anweshmishra on 09/02/20.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Path
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val delay : Long = 20
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
+
