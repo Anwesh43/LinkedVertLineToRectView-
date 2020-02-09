5abcf4f2d95c798df5ed1c5a7b0c5e3c9cd7cc43
Anwesh43 <anweshthecool0@gmail.com>
2020-02-09 19:26:05 +0530

    adding Int and Float extension methods

--- vertlinetorectview/src/main/java/com/anwesh/uiprojects/vertlinetorectview/VertLineTorRectView.kt
+++ vertlinetorectview/src/main/java/com/anwesh/uiprojects/vertlinetorectview/VertLineTorRectView.kt
@@ -21,3 +21,17 @@ val delay : Long = 20
 val foreColor : Int = Color.parseColor("#673AB7")
 val backColor : Int = Color.parseColor("#BDBDBD")
 
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), this.maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+//fun main() {
+//    //Math.min(1f / 2, 0.3f.maxScale(0, 2)) * 2
+//    Math.min(0.5f, Math.max(0f, 0.3f - 0 * 0.5f)) * 2
+//
+//    Math.min(0.5f, Math.max(0f, 0.3f)) * 2
+//    Math.min(0.5f, 0.3f) * 2
+//    0.3f * 2 -> 0.6
+//}
+
